1b16bdbf47245e8208c7ca3640817c2eedafbd38
olszzevska <138011777+olszzevska@users.noreply.github.com>
2023-06-29 04:03:37 +0200

    Delete assets directory

--- assets/js/email-popup.js
+++
@@ -1,8 +0,0 @@
-function fadeOutPopup() {
-            var popup = document.getElementById('popup-email');
-            popup.style.opacity = '0';
-            popup.style.top = '-100%';
-        }
-
-        // Trigger fade out after 6 seconds
-        setTimeout(fadeOutPopup, 6000);
